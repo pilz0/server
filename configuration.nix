96129e336b9be122667363b7ca3ded4c4502afcc
pilz0 <marie0@riseup.net>
2024-07-10 13:37:44 +0200

    fortnit

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,7 @@
       users.users.pizzladen.isNormalUser = true;
       users.users.pizzladen.description = "pizzaladen";
       users.users.pizzladen.createHome = true;
+      users.users.pizzaladen.isNormalUser = true;
       users.users.pizzaladen.home = "/home/pizzaladen";
       users.users.pizzaladen.createHome = true;
       users.users.pizzladen.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQpiMvv+hp/LcEV4olyPTfODg6OpOLXUYFETQuJx/D/ pizzaladen@pizzaladen-HP-EliteBook-840-G1" "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBTGgUYUsIAtcbZBqk5Mq0LH2T5KGFjdjAgNIwUf+/LBAAAABHNzaDo= pilz@framewok"];
