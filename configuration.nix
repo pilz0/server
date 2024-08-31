57bd92358547a9da795193f7ae20945c413f4a10
Pilz <48645439+pilz0@users.noreply.github.com>
2024-08-31 22:55:20 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -183,7 +183,7 @@ services.spotifyd.enable = true;
     programs.zsh.ohMyZsh.enable = true;
     programs.zsh.ohMyZsh.theme = "crunch";
     programs.zsh.autosuggestions.enable = true;
-    programs.zsh.shellAliases = { backup = "restic -r rclone:onedrive:/backup/server1 backup --verbose /home";};
+    programs.zsh.shellAliases = { backup = "restic -r rclone:smb:/Buro/backup backup -p /home/marie/restic/password --verbose /home /var/lib/docker /srv";};
     programs.zsh.shellAliases = { update = "sudo nix flake update /home/marie/server";};
     programs.zsh.shellAliases = { rebuild = "sudo nixos-rebuild --flake /home/marie/server switch";};
     users.defaultUserShell = pkgs.zsh;
