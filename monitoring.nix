e438ac7c6f1c1abc9beef0418b551bf3288e6847
pilz0 <marie0@riseup.net>
2024-11-04 21:30:17 +0100

    arrrr

--- monitoring.nix
+++ monitoring.nix
@@ -90,32 +90,37 @@
      exportarr-prowlarr = {
      enable = true;
      port = 51231;
-     apiKeyFile = "/home/marie/secrets/prowlarr";
+     apiKeyFile = "/var/lib/secrets/prowlarr";
+     user = "prometheus";
      url = config.services.nginx.virtualHosts."prowlarr.ketamin.trade".locations."/".proxyPass;
      };
      exportarr-lidarr = {
      enable = true;
      port = 56231;
-     apiKeyFile = "/home/marie/secrets/lidarr";
+     apiKeyFile = "/var/lib/secrets/lidarr";
+     user = "prometheus";
      url = config.services.nginx.virtualHosts."lidarr.ketamin.trade".locations."/".proxyPass;
      };
      exportarr-sonarr = {
      enable = true;
      port = 51211;
-     apiKeyFile = "/home/marie/secrets/sonarr";
+     apiKeyFile = "/var/lib/secrets/sonarr";
+     user = "prometheus";
      url = config.services.nginx.virtualHosts."sonarr.ketamin.trade".locations."/".proxyPass;
      };
      exportarr-radarr = {
      enable = true;
      port = 51711;
+     user = "prometheus";
      url = config.services.nginx.virtualHosts."radarr.ketamin.trade".locations."/".proxyPass;
-     apiKeyFile = "/home/marie/secrets/radarr";
+     apiKeyFile = "/var/lib/secrets/radarr";
      };
      exportarr-bazarr = {
      enable = true;
      port = 51235;
+     user = "prometheus";
      url = config.services.nginx.virtualHosts."bazarr.ketamin.trade".locations."/".proxyPass;
-     apiKeyFile = "/home/marie/secrets/bazarr";
+     apiKeyFile = "/var/lib/secrets/bazarr";
      };
     };
   };
