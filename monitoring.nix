64f94222ce84a3ac4f66fb71db35320777e7a16e
pilz0 <marie0@riseup.net>
2024-11-20 20:19:06 +0100

    tja

--- monitoring.nix
+++ monitoring.nix
@@ -46,7 +46,7 @@
         {
           name = "Loki";
           type = "loki";
-          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+          url = "http://127.0.0.1:3100";
           uid = "180d3e53-be75-4a6a-bb71-bdf437aec085";
         }
       ];
@@ -118,13 +118,12 @@
       group = "grafana";
       user = "grafana";
     };
-  };
   "grafana-dashboards/unpoller.json" = {
     source = ./grafana-dashboards/unpoller.json;
     group = "grafana";
     user = "grafana";
   };
-
+};
   services.prometheus = {
     scrapeConfigs = [
       {
@@ -231,83 +230,12 @@
   };
   services.loki = {
     enable = true;
-    configuration = {
-      server.http_listen_port = 3030;
-      auth_enabled = false;
-
-      ingester = {
-        lifecycler = {
-          address = "127.0.0.1";
-          ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-            replication_factor = 1;
-          };
-        };
-        chunk_idle_period = "1h";
-        max_chunk_age = "1h";
-        chunk_target_size = 999999;
-        chunk_retain_period = "30s";
-        max_transfer_retries = 0;
-      };
-
-      schema_config = {
-        configs = [
-          {
-            from = "2022-06-06";
-            store = "boltdb-shipper";
-            object_store = "filesystem";
-            schema = "v11";
-            index = {
-              prefix = "index_";
-              period = "24h";
-            };
-          }
-        ];
-      };
-
-      storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-          cache_location = "/var/lib/loki/boltdb-shipper-cache";
-          cache_ttl = "24h";
-          shared_store = "filesystem";
-        };
-
-        filesystem = {
-          directory = "/var/lib/loki/chunks";
-        };
-      };
-
-      limits_config = {
-        reject_old_samples = true;
-        reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = {
-        max_look_back_period = "0s";
-      };
-
-      table_manager = {
-        retention_deletes_enabled = false;
-        retention_period = "0s";
-      };
-
-      compactor = {
-        working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
-        compactor_ring = {
-          kvstore = {
-            store = "inmemory";
-          };
-        };
-      };
-    };
-  };
-
+    configFile = ./loki.yaml;
+    extraFlags = ["-config.expand-env=true"];
+};
   # promtail: port 3031 (8031)
   services.promtail = {
+    extraFlags = ["-config.expand-env=true"];
     enable = true;
     configuration = {
       server = {
@@ -319,7 +247,7 @@
       };
       clients = [
         {
-          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+          url = "http://127.0.0.1:3100/loki/api/v1/push";
         }
       ];
       scrape_configs = [
